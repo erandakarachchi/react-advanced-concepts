8c3b3db822d562828963d369d28ec4a3b298f9a6
Eranda K <emkarachchi@gmail.com>
2022-09-01 04:50:49 +0530

    Added sample code for dynamic imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [res, setRes] = useState(0);
+  // const add = async () => {
+  //   try {
+  //     const math = await import("./math");
+  //     const answer = math.addTwoNumbers(20, 40);
+  //     console.log("ANSWER : ", answer);
+  //     setRes(answer);
+  //   } catch (error) {
+  //     console.log("Unable to import math module");
+  //   }
+  // };
+
+  const add = () => {
+    import("./math")
+      .then((math) => {
+        const answer = math.addTwoNumbers(200, 300);
+        console.log("ANSWER : ", answer);
+        setRes(answer);
+      })
+      .catch((error) => {
+        console.log("Unable to import math module");
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => add()}>Add Two Numbers</button>
+      <h2>{res}</h2>
     </div>
   );
 }
