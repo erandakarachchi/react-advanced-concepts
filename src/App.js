a627b37720471e56617a168669fcb986ea3a908b
Eranda K <emkarachchi@gmail.com>
2022-09-01 08:27:45 +0530

    Lazy loading and error boundary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Suspense } from "react";
 
 function App() {
+  const HomeComponent = React.lazy(() => import("./HomeComponent"));
+  const DashboardComponent = React.lazy(() => import("./DashboardComponent"));
+
+  /**
+   * Must have suspense wrapper, otherwise app will not work.
+   * Can wrap multiple lazy loaded components in a single suspense element.
+   * Can give a fallback to show a loader.
+   * Can place error boundaries outside the wrapper to show errors when there is a failure.
+   */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>App Component</h2>
+      <Suspense fallback={<div>Loading....</div>}>
+        <HomeComponent />
+        <DashboardComponent />
+      </Suspense>
     </div>
   );
 }
